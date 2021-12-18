39d1e4a0574f90261e47a2487fe2a11330ba6541
amittras-pal <pal.amittras@gmail.com>
2021-12-18 10:00:05 +0530

    Login Form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,66 @@
 import React from "react";
+import * as Yup from "yup";
+import { useFormik, Field, FormikProvider } from "formik";
+import { Link } from "react-router-dom";
 import "./Login.scss";
 
 function Login() {
+  const loginForm = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Username is required!"),
+      password: Yup.string().required("Password is required!"),
+    }),
+    validateOnMount: true,
+    onSubmit: (values) => console.log(values),
+  });
+
   return (
-    <div className="login">
-      <h3 className="text-dark mb-4">Login</h3>
-      <form>
-        <input
-          type="text"
-          className="form-control form-control-sm"
-          placeholder="Username"
-          autoFocus
-        />
-      </form>
+    <div className="login-page row m-0 h-100">
+      {loginForm.valid}
+      <div className="col-md-6 col-lg-6 col-sm-12 pt-5">
+        <div className="login-container p-3">
+          <h3 className="text-dark pb-4 border-bottom border-light">
+            Login to Exxmon
+          </h3>
+          <FormikProvider value={loginForm}>
+            <form className="mt-4" onSubmit={loginForm.handleSubmit}>
+              <Field
+                name="username"
+                type="text"
+                className="form-control form-control-sm mb-3"
+                placeholder="Username"
+                autoFocus
+              />
+              <Field
+                name="password"
+                type="password"
+                className="form-control form-control-sm mb-3"
+                placeholder="Password"
+              />
+              <div className="d-flex justify-content-between align-items-center">
+                <span className="small">
+                  New to Exxmon?{" "}
+                  <Link
+                    to="/sign-up"
+                    className="text-primary text-decoration-none">
+                    Sign Up
+                  </Link>
+                </span>
+                <button
+                  type="submit"
+                  disabled={!loginForm.isValid}
+                  className="btn btn-sm btn-primary text-light fw-bold text-uppercase">
+                  Login
+                </button>
+              </div>
+            </form>
+          </FormikProvider>
+        </div>
+      </div>
     </div>
   );
 }
