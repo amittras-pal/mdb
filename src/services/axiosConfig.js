32686322773973d8df72b42ddbf56fc6a14b7fce
amittras-pal <pal.amittras@gmail.com>
2021-12-19 14:19:09 +0530

    API Auth through Bearer Token

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -4,12 +4,8 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  config.params = {
-    api_key: process.env.REACT_APP_API_KEY,
-    ...config.params,
-  };
-  return config;
-});
+instance.defaults.headers.common[
+  "Authorization"
+] = `Bearer ${process.env.REACT_APP_API_TOKEN}`;
 
 export default instance;
