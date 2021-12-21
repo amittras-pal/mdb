6e7245c877fec54f1f16df0dde9b7d750f20a99f
amittras-pal <pal.amittras@gmail.com>
2021-12-21 21:04:13 +0530

    Using API KEY instead of BEARER TOKEN for now.

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -4,8 +4,13 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-instance.defaults.headers.common[
-  "Authorization"
-] = `Bearer ${process.env.REACT_APP_API_TOKEN}`;
+instance.interceptors.request.use((config) => {
+  config.params = { api_key: process.env.REACT_APP_API_KEY, ...config.params };
+  return config;
+});
+
+// instance.defaults.headers.common[
+//   "Authorization"
+// ] = `Bearer ${process.env.REACT_APP_API_TOKEN}`;
 
 export default instance;
